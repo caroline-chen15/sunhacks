647eab76c0eb8a5a34e3100642ccfa00dc78e187
caroline-chen15 <49343443+caroline-chen15@users.noreply.github.com>
2020-08-29 14:10:17 -0400

    map marker

--- app.py
+++ app.py
@@ -8,8 +8,16 @@ app = Flask(__name__)
 @app.route("/")
 
 def base():
-    map = folium.Map(location = [45.52356, -122.6750])
+    map = folium.Map(location = [38.031572, -78.510631])
+    
+    folium.Marker(
+        location = [38.031572, -78.510631],
+        popup = "<b>Hacks of Kindness</b>\n Oct 3, 2020 \n ",
+        tooltip = "Hacks of Kindness"
+    ).add_to(map)
+
     return map._repr_html_()
 
 if __name__ == "__app__":
     app.run(debug=True)
+
